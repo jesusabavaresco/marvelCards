e48c40ec88face9db38a564b20cde186943e5348
jesus bavaresco <jesusbavaresco@gmail.com>
2024-01-11 17:36:25 -0600

    first commit

---
+++ server/src/models/Teams.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('teams', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // lastName: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false,
+    // },
+    // description: {
+    //   type: DataTypes.TEXT,
+    //   allowNull: false,
+    // },
+    // image: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false
+    // },
+    // nationality: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false
+    // },
+    // birthDate: {
+    //   type: DataTypes.STRING,
+    //   allowNull: false
+    // },
+    // createdinDb: {
+    //   type: DataTypes.BOOLEAN,
+    //   allowNull: false,
+    //   defaultValue: true
+    // },
+  }, { timestamps: false });
+};
+
---
+++ server/src/routes/index.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+// const driversRoutes = require('./driversRoutes')
+// const teamsRoutes = require('./teamsRoutes')
+
+const router = Router();
+
+// router.use('/drivers', driversRoutes);
+// router.use('/teams', teamsRoutes);
+
+
+module.exports = router;
